0a66224fa7929e9d1e72806665dda17d8d6c0ab7
Ethan chen <13531194616@163.com>
2020-06-19 11:00:22 +0800

    create project

---
+++ chapter1/chapter1/test_main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+int main()
+{
+	/*
+		* 标准库定义的所有名字都在命名空间std中
+		* ::为作用域运算符
+	*/
+	//std::cout << "Hello, World!" << std::endl;
+
+	//计算两个数之和
+	/*
+		std::cout << "Enter two numbers:" << std::endl;
+		int v1 = 0, v2 = 0;
+		std::cin  >> v1 >> v2;
+		std::cout << "The sum of " << v1 << " and " << v2
+			<< " is " << v1 + v2 << std::endl;
+	*/
+	
+	// 计算两个数之积
+		/*
+			std::cout << "Enter two numbers:" << std::endl;
+			int v1 = 0, v2 = 0;
+			std::cin >> v1 >> v2;
+			std::cout << "The product of " << v1 << "and " << v2
+				<< " is " << v1 * v2 << std::endl;
+		*/
+
+	std::cout << /* "*/" /* "/*" */;
+	return 0;
+}
